1313cc0088cb576e983cb8cb8d2dfe83d9746ba9
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-07-31 19:00:01 -0300

    closes #4 - criação do projeto java

---
+++ estudante-backend/estudante-backend/src/main/java/com/estudante/estudante_backend/EstudanteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estudante.estudante_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudanteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudanteBackendApplication.class, args);
+	}
+
+}
---
+++ estudante-backend/estudante-backend/src/test/java/com/estudante/estudante_backend/EstudanteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudante.estudante_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudanteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
